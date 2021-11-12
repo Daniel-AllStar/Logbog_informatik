ef489e6eb8696fad65eaf7b20a0b3e8a5f88203c
Daniel-AllStar <danielhaddad58@gamil.com>
2021-11-12 22:22:38 +0100

    Initial commit

---
+++ Logbog.py
@@ -0,0 +1,21 @@
+starten snakkede vi om server og hvordan internettet hænger sammen, vi fik også snakket meget om Marks server i holland,
+
+
+
+Uge 44
+
+Vi startede ud med at lege med rotor, som vi skulle anaysere og lege lidt med koder, hvor så at vi skulle finde ud af hvordan man bruger dem i programmering.
+
+Tordag, legede jeg lidt med 3d-modeler og 3d-print, hvor så at jeg skulle 3d printe min bærbar holder, men jeg havde lige en sjov lærer som lånte/nakkede.
+Da jeg kom hjem så benchmarkede jeg min bærbar, uden noget under den, hvor den ligger ned på bordet, så blev den følgene:
+
+unden noget under den = max.temp 76
+blyanter under = max.temp 75
+3d-printet bærbar holder = max.temp 69-70
+
+jeg er kommet frem til at den har hjulpet min bærbars temp
+
+
+uge 45
+
+vi har fortsat med at leget med robotten, men jeg har taget den med hjem så jeg kunne lege med dens kode når jeg kommer hjem.
